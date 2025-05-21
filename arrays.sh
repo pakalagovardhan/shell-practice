be8c95c4852ffb3abd98ad27b0d94b46f9426c7f
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-21 17:22:31 +0530

    few lines added

--- arrays.sh
+++ arrays.sh
@@ -3,3 +3,7 @@
 movies=("Hit2" "Thandel" "Pushpa2" "Court")
 
 echo "First movies is : ${movies[0]}"
+echo "second movies is : ${movies[1]}"
+echo "All movies is : ${movies[@]}"
+echo "Fifth movies is : ${movies[5]}"
+
