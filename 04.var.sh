327c5b03db2ceeb1c1d734a81da401398dc292c8
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-21 16:24:31 +0530

    few lines added

--- 04.var.sh
+++ 04.var.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=modi      #no space between = before and after
-PERSON2=Musk
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: hi, $PERSON2"
 echo "$PERSON2 :: hello $PERSON1,am good.How are you"
