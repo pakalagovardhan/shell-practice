bf95fbfffbb084ef7ac4acb7c9cb14df42e252e2
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-21 16:14:05 +0530

    few lines added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: hi, $PERSON2"
+echo "$PERSON2 :: hello $PERSON1,am good.How are you"
+echo "$PERSON1:: Good!"
+echo "$PERSON2 :: Bye $PERSON1"
+echo "$PERSON1:: Ok!"
