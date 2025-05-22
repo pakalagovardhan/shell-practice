9545c9e743aaf9b3dccae815c2f94ba00dc2ba56
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-22 13:08:01 +0530

    linux commands

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed: $@"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running script: $USER"
+echo "home directory user: $HOME"
+echo "PID of script: $$"
+echo "PID in last command: $!"
