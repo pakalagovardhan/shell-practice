e523ff6fdb941b2cce95ed8b4beb0f3499ae7ae0
pakalagovardhan <p.govardhan321@gmail.com>
2025-05-22 14:31:49 +0530

    linux commands

---
+++ 10-condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USER -ne 0]
+then
+    echo "ERROR:: Please run the script with root access"
+else
+    echo "you are running with root access"
+fi
+
+dnf install mysql -y
